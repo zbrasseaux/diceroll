0c22043b1879bfcfcfc33ffa7977b613fdb2d96b
zbrasseaux <zbrasseaux97@gmail.com>
2019-04-10 23:24:31 -0500

    fixed setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 echo "Copying dice.py to /usr/bin directory."
 sudo cp ./dice /usr/bin/dice
+
+echo "Changing permissiond of /usr/bin/dice."
+sudo chmod 777 /usr/bin/dice
+
 echo "Setup complete."
