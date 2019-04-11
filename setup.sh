3932a1406e51fb2d3e1e094184f1a0bafcb7df3e
zbrasseaux <zbrasseaux97@gmail.com>
2019-04-10 23:25:35 -0500

    fixed typo in comment

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "Copying dice.py to /usr/bin directory."
 sudo cp ./dice /usr/bin/dice
 
-echo "Changing permissiond of /usr/bin/dice."
+echo "Changing permission of /usr/bin/dice."
 sudo chmod 777 /usr/bin/dice
 
 echo "Setup complete."
